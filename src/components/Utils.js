55cf7a72429124ebb7c74eaca6c8a19c2e61fbdd
Rahul <rkredux@gmail.com>
2017-06-27 11:38:12 -0400

    Add getDateString (to get today's date) and degToDirection (to parse wind direction into compass direction)

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,4 +1,40 @@
 
-function timeToDate(timeStamp){
-	
+export function getDateString(){
+	const weekDayMap = {
+	  0: "Sun", 
+	  1: "Mon", 
+	  2: "Tue", 
+	  3: "Wed", 
+	  4: "Thu", 
+	  5: "Fri", 
+	  6: "Sat"
+	}
+	const monthNameMap = {
+	  0: "Jan", 
+	  1: "Feb",
+	  2: "Mar",
+	  3: "Apr",
+	  4: "May", 
+	  5: "June", 
+	  6: "Jul", 
+	  7: "Aug", 
+	  8: "Sep", 
+	  9: "Oct", 
+	  10: "Nov", 
+	  11: "Dec"
+	}
+	const time = new Date(); 
+	const day = time.getDate();
+	const dayName = weekDayMap[time.getDay()]; 
+	const monthName = monthNameMap[time.getMonth()]; 
+	return `${dayName}, ${day} ${monthName}`
 }
+
+
+
+export function degToDirection(degrees){
+    const val = parseInt((degrees/22.5)+.5); 
+    const directionList = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]; 
+    return directionList[(val % 16)]; 
+}
+
