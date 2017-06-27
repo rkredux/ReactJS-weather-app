ef5e909931d8961926121f206eb0977c64a7ae92
Rahul <rkredux@gmail.com>
2017-06-27 11:37:07 -0400

    Add wind direction props

--- src/components/Conditions.js
+++ src/components/Conditions.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { degToDirection } from "./Utils.js"; 
 
 
 class Conditions extends Component{
 	render(){
 		return(
 			<div className="row thirdrow">
-			  <p>{this.props.wind}</p>
+			  <p>{this.props.wind} {degToDirection(this.props.windDirection)}</p>
 			  <p>{this.props.humidity}</p>
 			  <p>{this.props.humidity}</p>
 			</div>
